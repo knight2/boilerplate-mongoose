c17769c8444cb97403ef3c287d49771de7aa24c8
Knight2 <8440420+knight2@users.noreply.github.com>
2018-12-10 18:28:19 -0500

    find model by _id

--- myApp.js
+++ myApp.js
@@ -157,9 +157,10 @@ var findPeopleByName = function(personName, done) {
 // argument `food` as search key
 
 var findOneByFood = function(food, done) {
-
-  done(null/*, data*/);
-  
+  Person.findOne({favoriteFoods: [food]}, (err, data) =>{
+    if (err) return done(err);
+    done(null, data);
+  })
 };
 
 /** 7) Use `Model.findById()` */
@@ -172,9 +173,10 @@ var findOneByFood = function(food, done) {
 // Use the function argument 'personId' as search key.
 
 var findPersonById = function(personId, done) {
-  
-  done(null/*, data*/);
-  
+  Person.findById(personId, (err, data) =>{
+    if (err) return done(err);
+    done(null, data);
+  })
 };
 
 /** # CR[U]D part III - UPDATE # 
